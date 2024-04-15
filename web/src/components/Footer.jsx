29eb5d5181745cc54dfb09575a614b130d1d04c3
Sai Kumar <sai@umee.cc>
2024-04-15 18:57:48 +0530

    added url and api on footer

--- web/src/components/Footer.jsx
+++ web/src/components/Footer.jsx
@@ -1,7 +1,13 @@
 export default function Footer() {
     return (
-        <div className="container root-bg ubuntu-light py-3 my-4 border-top">
+        <div className="container">
+            <div className="root-bg ubuntu-light py-3 my-4 border-top">
+                <p>RPC: <a target="_blank" className="text-black no-anchor" href="https://umee-rpc.polkachu.com/">https://umee-rpc.polkachu.com</a></p>
+                <p>LCD: <a target="_blank" className="text-black no-anchor" href="https://umee-api.polkachu.com/">https://umee-api.polkachu.com</a></p>
+            </div>
+            <div className="root-bg ubuntu-light py-3 my-4">
             Created by the <img src="https://app.ux.xyz/favicon.png" width={16}></img> Umee Team &middot; &copy; 2024
+            </div>
         </div>
     )
 }
